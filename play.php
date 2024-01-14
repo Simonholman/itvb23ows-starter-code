01e64d222d71932a8f6849b29d41861ab36206e6
Simon <simonholman@hotmail.com>
2024-01-14 16:22:02 +0100

    snake_case to camelCase

--- play.php
+++ play.php
@@ -34,7 +34,7 @@ elseif (array_sum($hand) <= 8 && $hand['Q']) {
     $stmt = $db->prepare(
         'insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)'
     );
-    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], get_state());
+    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], getState());
     $stmt->execute();
     $_SESSION['last_move'] = $db->insert_id;
 }
